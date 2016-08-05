a958108e4c8d8cebff9a61add9b168f3058df524
Tran Tan Lam <trankind@gmail.com>
2016-08-05 15:01:23 +0700

    Add invite friend storyboard

---
+++ GoalPerformance/ViewControllers/InviteFriendViewControllers/SelectInviteFriendViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SelectInviteFriendViewController.swift
+//  GoalPerformance
+//
+//  Created by Lam Tran on 8/5/16.
+//  Copyright © 2016 Group 7. All rights reserved.
+//
+
+import UIKit
+
+class SelectInviteFriendViewController: UIViewController {
+
+    @IBOutlet weak var suggestFriendTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
