332dc8ea79bbea8dcfcbb16bf629ebe50eb41b52
Tran Tan Lam <trankind@gmail.com>
2016-08-05 15:15:17 +0700

    Init InviteFriend storyboard

---
+++ GoalPerformance/Views/suggestFriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  suggestFriendTableViewCell.swift
+//  GoalPerformance
+//
+//  Created by Lam Tran on 8/5/16.
+//  Copyright © 2016 Group 7. All rights reserved.
+//
+
+import UIKit
+
+class suggestFriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
