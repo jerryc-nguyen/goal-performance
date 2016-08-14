bcb39203f5a840c269075cb881ad611d314edd57
sophie <sophievictor55@gmail.com>
2016-08-14 11:52:28 +0700

    complete send data

--- GoalPerformance/ViewControllers/NewGoalViewControllers/DefineGoalViewController.swift
+++ GoalPerformance/ViewControllers/NewGoalViewControllers/DefineGoalViewController.swift
@@ -15,6 +15,8 @@ class DefineGoalViewController: UIViewController, DurationViewControllerDelegate
     var timeChosen: String = ""
     var weekdays:[String] = []
     var duration:Int = 0
+    var categoryID:Int? = 0
+    var categoryName:String? = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,52 +26,23 @@ class DefineGoalViewController: UIViewController, DurationViewControllerDelegate
         self.navigationItem.rightBarButtonItem = nextBarItem
     }
     
-    override func viewWillDisappear(animated: Bool) {
-        timePickerAction(startTimePicker)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-    
-    }
-    
     func done() {
-        
-        
-        
-//        goal[name]:Goal name 1
-//        goal[start_at]:06:30 PM
-//        goal[repeat_every][]:monday
-//        goal[repeat_every][]:tuesday
-//        goal[repeat_every][]:thursday
-//        goal[repeat_every][]:friday
-//        goal[repeat_every][]:sunday
-//        goal[duration]:10
-//        goal[sound_name]:'0'
-//        goal[is_challenge]:true
-//        goal[is_default]:true
-//        goal[category_id]:6
-        
-
-        let params : [String : AnyObject] = [
-            "goal[name]" : "Swim",
+       let params : [String : AnyObject] = [
             "goal[start_at]" : self.timeChosen,
             "goal[repeat_every]" : self.weekdays,
             "goal[duration]" : self.duration,
-            "goal[sound_name]" : 0,
+            "goal[sound_name]" : "alarm1",
             "goal[is_challenge]" : true,
             "goal[is_default]" : true,
-            "goal[category_id]" : 6
+            "goal[category_id]" : self.categoryID!
         ]
-        
         APIClient.sharedInstance.sendSetupGoalData(params) { (result) in
             print(result)
         }
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func timePickerAction(sender: UIDatePicker) {
@@ -90,8 +63,8 @@ class DefineGoalViewController: UIViewController, DurationViewControllerDelegate
             let goalIntervalTableVC = segue.destinationViewController as! GoalIntervalTableViewController
             goalIntervalTableVC.parentScreen = self
         }
-        
     }
+    
 
     func durationViewController(durationVC: DurationViewController, durationUpdated duration: Int) {
         print("get duration: \(duration)")
@@ -101,6 +74,5 @@ class DefineGoalViewController: UIViewController, DurationViewControllerDelegate
     func weekdaysViewController(weekdayVC: WeekdaysViewController, weekdays: [String]) {
         print("get weekdays: \(weekdays)")
         self.weekdays = weekdays
-        
-    }
+   }
 }
