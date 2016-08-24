272ea3bdca6c6a349171070df8cc76ebc31feecf
sophie <sophievictor55@gmail.com>
2016-08-24 22:34:17 +0700

    fix page menu

--- GoalPerformance/ViewControllers/FriendControllers/FriendViewController.swift
+++ GoalPerformance/ViewControllers/FriendControllers/FriendViewController.swift
@@ -45,6 +45,7 @@ class FriendViewController: UIViewController, CAPSPageMenuDelegate {
         controllerArray.append(controller3)
         
         let parameters: [CAPSPageMenuOption] = [
+            
             .ScrollMenuBackgroundColor(UIColor.whiteColor()),
             .ViewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
             .BottomMenuHairlineColor(UIColor(red: 255.0/255.0, green: 87.0/255.0, blue: 34.0/255.0, alpha: 0.5)),
@@ -53,123 +54,13 @@ class FriendViewController: UIViewController, CAPSPageMenuDelegate {
             .MenuHeight(40.0),
             .SelectedMenuItemLabelColor(UIColor(red: 255.0/255.0, green: 87.0/255.0, blue: 34.0/255.0, alpha: 1.0)),
             .UnselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
-            .MenuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 14.0)!),
+            .MenuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 12.0)!),
             .SelectionIndicatorHeight(2.0),
         ]
         
-        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, UIScreen.mainScreen().bounds.size.width, self.view.frame.height), pageMenuOptions: parameters)
         self.view.addSubview(pageMenu!.view)
     }
 
-//    func loadFriend() {
-//        
-//        if viewSegment.selectedSegmentIndex == 0 {
-//            apiClient.getPendingFriend { (friends) in
-//                self.pendingFriends = friends
-//                self.friendTableView.reloadData()
-//            }
-//            
-//            apiClient.getAllFriends { (friends) in
-//                self.friends = friends
-//                self.friendTableView.reloadData()
-//            }
-//        } else {
-//            apiClient.getPendingFriend { (friends) in
-//                self.pendingFriends = friends
-//                self.friendTableView.reloadData()
-//            }
-//            
-//            apiClient.getAllFriends { (friends) in
-//                self.friends = friends
-//                self.friendTableView.reloadData()
-//            }
-//            
-//        }
-//    }
-    
-//    @IBAction func onSegmentChange(sender: UISegmentedControl) {
-//        loadFriend()
-//    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
-
-//extension FriendViewController: UITableViewDelegate, UITableViewDataSource {
-//    
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 2
-//    }
-//    
-//    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        switch section {
-//        case 0:
-//            
-//            if pendingFriends.count > 0 {
-//                if viewSegment.selectedSegmentIndex == 0 {
-//                    return "Pending friends"
-//                } else {
-//                    return "Pending buddies"
-//                }
-//            } else {
-//                return ""
-//            }
-//                
-//            
-//        default:
-//            if viewSegment.selectedSegmentIndex == 0 {
-//                return "List friends"
-//            } else {
-//                return "List buddies"
-//            }
-//        }
-//    }
-//    
-//    
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch section {
-//        case 0:
-//            return pendingFriends.count
-//            
-//        case 1:
-//            return friends.count
-//            
-//        default:
-//            return 0
-//        }
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("FriendTableViewCell") as! FriendTableViewCell
-//        
-//        cell.apiClient = apiClient
-//        
-//        var friend: User?
-//        
-//        switch indexPath.section {
-//        case 0:
-//            friend = pendingFriends[indexPath.row]
-//            cell.challengeImage.hidden = true
-//            
-//        default:
-//            friend = friends[indexPath.row]
-//            cell.acceptButton.hidden = true
-//            cell.rejectButton.hidden = true
-//        }
-//        
-//        cell.friend = friend
-//        
-//        return cell
-//        
-//    }
-//}
-
